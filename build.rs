e722e6d6cde4ebdc9fc1cd6c22babd26717cd5ac
Trash Panda <trashpanda@trashband.it>
2025-06-11 22:58:06 -0700

    Update manifest embedding

--- build.rs
+++ build.rs
@@ -1,30 +1,14 @@
-use std::io::Write;
-
-use winres;
+use embed_manifest::manifest::{ExecutionLevel, Setting};
+use embed_manifest::{embed_manifest, new_manifest};
 
 fn main() {
-    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
-        let mut res = winres::WindowsResource::new();
-        res.set_windres_path("/usr/bin/x86_64-w64-mingw32-windres")
-            .set_manifest(
-                r#"
-                    <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
-                        <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
-                            <security>
-                                <requestedPrivileges>
-                                    <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
-                                </requestedPrivileges>
-                            </security>
-                        </trustInfo>
-                    </assembly>
-                "#,
-            );
-        match res.compile() {
-            Err(error) => {
-                write!(std::io::stderr(), "{}", error).unwrap();
-                std::process::exit(1);
-            }
-            Ok(_) => {}
-        }
+    if std::env::var_os("CARGO_CFG_WINDOWS").is_some() {
+        embed_manifest(
+            new_manifest("dropship-rs")
+                .long_path_aware(Setting::Enabled)
+                .requested_execution_level(ExecutionLevel::RequireAdministrator),
+        )
+        .expect("unable to embed manifest file");
     }
+    println!("cargo:rerun-if-changed=build.rs");
 }
